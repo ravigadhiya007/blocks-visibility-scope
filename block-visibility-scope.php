296aff99e7ba8a60a7f82d23adacbb94971bd634
Ravi Gadhiya <ravigadhiya007@gmail.com>
2025-09-10 19:12:10 +0530

    Initial commit

---
+++ block-visibility-scope.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:         Block Visibility Scope
+ * Plugin URI:          #
+ * Description:         Easily manage block visibility across different screen sizes in the WordPress block editor.
+ * Version:             1.0.0
+ * Requires at least:   6.3
+ * Requires PHP:        7.4
+ * Author:              Ravi Gadhiya
+ * Author URI:          #
+ * License:             GPLv2
+ * License URI:         https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain:         block-visibility-scope
+ * Domain Path:         /languages
+ *
+ * @package block-visibility-scope
+ */
+
+defined( 'ABSPATH' ) || exit;
